f3a5fdeb2e92a7b1b6b697703cba724616e727b5
Marcos Cangussu <marcos.cangussu@hotmail.com>
2024-10-18 08:59:14 -0300

    token

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://10.0.2.2:3333/',
   headers: {
     Authorization:
-      'Bearer MQ.zFHGVf1n10gL5-drMj8OIUBHmU-TwUyhaUL8y4kZZmNlXiXSvhWUSVYdhEXH',
+      'Bearer MQ.ohM2MTseh0xKX2HfEAVwuBBqLAOH4nxW5zBHYTuMV3KGEqi6QMBndqmxM5Fv',
   },
 });
